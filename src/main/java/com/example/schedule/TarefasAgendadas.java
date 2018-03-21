0b301ae11f627b98e3f2b0317494ed0ca71480a7
Luiz Alberto <luiz.trisoft@gmail.com>
2018-03-20 22:57:00 -0300

    Configuração do pool

--- src/main/java/com/example/schedule/TarefasAgendadas.java
+++ src/main/java/com/example/schedule/TarefasAgendadas.java
@@ -1,7 +1,5 @@
 package com.example.schedule;
 
-
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,17 +10,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TarefasAgendadas {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ScheduleApplication.class);
 	private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-	
+
 	@Scheduled(fixedRate = 1000)
 	public void tarefaAgendada() {
-		logger.info("Tarefa executando a cada 1 segundo... " + dateTimeFormatter.format(LocalDateTime.now())); 
+		logger.info("Tarefa executando a cada 1 segundo... " + dateTimeFormatter.format(LocalDateTime.now()));
 	}
-	
+
 	@Scheduled(fixedRate = 1000, initialDelay = 5000)
 	public void tarefaAgendadaComAtrasoNaInicializacao() {
-		logger.info("Atraso inicial... " + dateTimeFormatter.format(LocalDateTime.now())); 
+		logger.info("Atraso inicial de 5 segundos... " + dateTimeFormatter.format(LocalDateTime.now()));
 	}
 }
